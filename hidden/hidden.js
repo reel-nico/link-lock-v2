de6553863e3ef5026116f3e521c64f8e421e1763
りぃる <100053953+reel-nico@users.noreply.github.com>
2022-08-12 18:52:16 +0900

    Update hidden.js

--- hidden/hidden.js
+++ hidden/hidden.js
@@ -148,7 +148,7 @@ async function randomLink() {
 function main() {
   if (window.location.hash) {
     document.querySelector("#encrypted-url").value =
-      `https://jstrieb.github.io/link-lock/${window.location.hash}`;
+      `https://www2.reel-lock.ml/${window.location.hash}`;
 
     window.location.hash = "";
   }
